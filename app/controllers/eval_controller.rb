7f9f05efaf840eafb6727f5fe0b34f426830f1ed
Alex Epifanov <aepifanov@tau-technologies.com>
2017-04-16 02:04:01 +0300

    allow origins

--- app/controllers/eval_controller.rb
+++ app/controllers/eval_controller.rb
@@ -13,6 +13,11 @@ class EvalController < ApplicationController
   		result['error'] = e.to_s
   	end
 
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
   	render :json => result  	
   end
 end
